9744e530beb4ca6a78b86fc0a4d58f5fdac95b91
Manuel Gallego <manuel.gallego@live.u-tad.com>
2023-10-25 17:12:42 +0200

    Commit comentarios

--- src/correctorConsola/Main.java
+++ src/correctorConsola/Main.java
@@ -3,23 +3,30 @@ package correctorConsola;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Corrector corrector = new Corrector();
-        corrector.setPriority(Thread.MIN_PRIORITY);
-        corrector.start();
+	public static void main(String[] args) {
+		// Instancia de la clase corrector
+		Corrector corrector = new Corrector();
+		// Establecemos la prioridad del hilo al minimo
+		corrector.setPriority(Thread.MIN_PRIORITY);
+		// Inicia el corrector
+		corrector.start();
 
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Escribe una palabra (o 'Fin' para terminar):");
-            String palabra = scanner.nextLine();
+		while (true) {
+			// Solicita una palabra al usuario
+			System.out.println("Escribe una palabra (o 'Fin' para terminar):");
+			String palabra = scanner.nextLine();
 
-            if (palabra.equalsIgnoreCase("Fin")) {
-                corrector.pararHilo();
-                break;
-            }
+			// Se comprueba si el usuario quiere cerrar el programa
+			if (palabra.equalsIgnoreCase("Fin")) {
+				// Se detiene el hilo y se sale del bucle
+				corrector.pararHilo();
+				break;
+			}
 
-            corrector.setEntrada(palabra);
-        }
-    }
+			// Se establece la palabra a corregir
+			corrector.setEntrada(palabra);
+		}
+	}
 }
